3aa4ca232e36780bbaba513d8586891963837ba1
Abraham Silva Mendez <spiderhomesi23@gmail.com>
2026-07-02 23:00:12 -0600

    Correccion ejercicio 4

--- ejercicio_4/Text.py
+++ ejercicio_4/Text.py
@@ -3,9 +3,19 @@ from antlr4 import *
 
 from ExprLexer import ExprLexer
 
-# Lo quw obtienes es la entrada, analiza el texto y lo separa en tokens
-lexer = ExprLexer(InputStream(input("? ")))
+import sys
 
+import os
+
+if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
+    # Si el archivo existe, lo procesa como un archivo
+    input_stream = FileStream(sys.argv[1])
+else:
+    # Si no existe el archivo, lo procesa como texto directo
+    input_stream = InputStream(input("? "))
+
+# Lo que obtienes es la entrada, analiza el texto y lo separa en tokens
+lexer = ExprLexer(input_stream)
 # Toma los tokens que produjo el lexer 
 tokens = CommonTokenStream(lexer)
 tokens.fill()
@@ -16,9 +26,8 @@ for token in tokens.tokens:
     print("Texto ", token.text)
     print("Tipo ", token.type)
     print("Linea ", token.line)
-    print("columna ", token.column)
-    nombre_token = lexer.symbolicNames[token.type]
+    print("Columna ", token.column)
+    nombre = lexer.symbolicNames[token.type]
+    print("Nombre ", nombre)
 
-    print("Nombre del token: ", nombre_token)
-
-    print("---------------------")
+    print("--------------------------------------------------")
